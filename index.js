35e31b528bfe21889c7587493239eddda1afa9c1
Matthew Etress <mattetress@me.com>
2018-08-28 04:36:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,19 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
+
+
+function sayHiToGrandma(string) {
+  var upperCase = "HELLO!";
+  var lowerCase = "hello!";
+  var loveYou = "I love you, Grandma.";
+  
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!";
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!";
+  } else if (string === loveYou) {
+    return "I love you, too.";
+  }
+  }
+}
